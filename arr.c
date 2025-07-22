20cad85562fea103e21b5aa22c769d0b3c50557f
sinchana <Sinchanaim2@gmail.com>
2025-07-22 21:34:46 +0530

    Update array.c

--- arr.c
+++ arr.c
@@ -5,14 +5,17 @@ int main()
     int arr[5], i;
 
     // Input 5 elements
+    printf("Enter 5 integers:\n");
     for (i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
 
     // Output 5 elements
+    printf("You entered: ");
     for (i = 0; i < 5; i++) {
         printf("%d ", arr[i]);
     }
 
+    printf("\n");
     return 0;
 }
