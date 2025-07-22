916d41313cd2c5fed1a666b2850e6e0be722df3e
sinchana <sinchanaim2@gmail.com>
2025-07-22 23:21:21 +0530

    python programs

---
+++ python/largest.py
@@ -0,0 +1,7 @@
+# Input: array of numbers
+arr = list(map(int, input("Enter the elements separated by space: ").split()))
+
+# Find the largest element
+largest = max(arr)
+
+print("The largest element is:", largest)
